7e3188777b6077ad68591812a3340620306d928f
Orbit X <153139012+0rbitX@users.noreply.github.com>
2025-01-06 11:32:57 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -26,4 +26,11 @@ for (const tab of tabs) {
   elements.add(element);
 }
 document.querySelector("ul").append(...elements);
-...
+const button = document.querySelector("button");
+button.addEventListener("click", async () => {
+  const tabIds = tabs.map(({ id }) => id);
+  if (tabIds.length) {
+    const group = await chrome.tabs.group({ tabIds });
+    await chrome.tabGroups.update(group, { title: "DOCS" });
+  }
+})
